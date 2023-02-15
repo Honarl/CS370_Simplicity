662b96e99b318de17f066b5138111c1a93a5d6fc
Honarl <rogersmq@sunypoly.edu>
2023-02-15 14:19:38 -0500

    Libraries (no rogueutil yet)

--- Simplicity.h
+++ Simplicity.h
@@ -1,5 +1,12 @@
 #ifndef SIMPLICITY_H_INCLUDED
 #define SIMPLICITY_H_INCLUDED
+#include <stack>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <limits>
+#include <typeinfo>
+#include <sstream>
 
 
 
