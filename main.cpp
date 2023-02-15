10f5560044c849ed635e2d116233baade1e205f0
Honarl <rogersmq@sunypoly.edu>
2023-02-15 15:09:04 -0500

    Added Intents for funny

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 
 int main (void){
 
+    std::cout<<"System Intent: Visualize data structures that students will use in CS 240 in a cross-platform, easy to install C++ library"<<"\n";
+
+    std::cout<<"Cycle Intent: Visualize Arrays, Stacks, and Queues from a CS 240 student's program graphically, using ASCII characters, in a simple and easy-to-understand format";
+
     return EXIT_SUCCESS;
 }
