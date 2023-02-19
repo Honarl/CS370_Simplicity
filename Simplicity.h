2baa3d962bad3ad83114cb3c6be806d289f64704
BetCKamas <kamase@sunypoly.edu>
2023-02-19 15:32:41 -0500

    comments
    commented my code

--- Simplicity.h
+++ Simplicity.h
@@ -13,74 +13,65 @@ namespace simplicity{
 	class simple{
 			private:
 				static void printTop(int length, int width){
-					std::cout<<"+";
-					for(int j = 0; j < length; j++){
-						for(int i = 0; i < (width + 2); i++){
+					std::cout<<"+";								// printing leftmost corner
+					for(int j = 0; j < length; j++){			// loop to end current member box
+						for(int i = 0; i < (width + 2); i++){	// loop to print width 
 							std::cout<<"-";
 						}
 						std::cout<<"+";
 					}
-					std::cout<<std::endl;
+					std::cout<<std::endl;						// ending top structure
 				}
 				
 				template <typename T>
 				static void printContent(T content[], int length, int width){
-					std::cout<<"|";
-					for(int i = 0; i < length; i++){
-						std::cout<<" ";
-						std::cout<<std::setw(width);
-						std::cout<<content[i];
-						std::cout<<" |";
+					std::cout<<"|";									// Leftmost member box
+					for(int i = 0; i < length; i++){				// loop to print whitespace and member
+						std::cout<<" "<<std::setw(width);
+						std::cout<<content[i]<<" |";
 					}
-					std::cout<<std::endl;
+					std::cout<<std::endl;							// ending content line
 				}
 				
 				static void printBottom(int length, int width){
-					std::cout<<"+";
-					for(int j = 0; j < length; j++){ 
-						for(int i = 0; i < (width + 2); i++){
+					std::cout<<"+";										// ending leftmost corner
+					for(int j = 0; j < length; j++){ 					// loop to print corner marker
+						for(int i = 0; i < (width + 2); i++){			// loop to print bottom line
 							std::cout<<"-";
 						}
 						std::cout<<"+";
 					}
-					std::cout<<std::endl;
-					for(int i = 0; i < length; i++){
+					std::cout<<std::endl;								// ending bottom of structure
+					
+					for(int i = 0; i < length; i++){					// loop to print indexs
 						std::cout<<" ";
-						int halfway = 0;
-						if((width+2) % 2 == 0){
-							halfway = (width / 2);
-							std::cout<<" ";
-							std::cout<<std::setw(halfway)<<" "<<i;
-							
-							for(int j = 0; j < halfway; j++)
+						int halfway = 0;								// initializing to zero
+						if((width+2) % 2 == 0){							// checking if member takes up an even number of spaces
+							halfway = (width / 2);						// setting midpoint
+							std::cout<<" "<<std::setw(halfway)<<" "<<i;	// printing index with appropiate whitespace
+							for(int j = 0; j < halfway; j++)			// loop to finish printing out whitespace
 								std::cout<<" ";	
 						}
-						else{
-							halfway = (width / 2) +1 ; 
-
-							std::cout<<" ";
-							std::cout<<std::setw(halfway)<<i;
-							
-							for(int j = 0; j < halfway; j++)
+						else{											// widest data must take up an odd number of spaces
+							halfway = (width / 2) + 1 ; 				// setting halfway point and rounding up
+							std::cout<<" "<<std::setw(halfway)<<i;		// printing whitespace and index
+							for(int j = 0; j < halfway; j++)			// loop to finish printing whitespace
 								std::cout<<" ";		
 						}
-						
-						//std::cout<<" ";
 					}
-					
-					std::cout<<std::endl;
+					std::cout<<std::endl;								// end index line
 				}
 				
 				template <typename T>
 				static int widestMember(T content[], int len){
-					int widest = 1;
-					for(int g = 0; g < len; g++){
-						int width = 1;
+					int widest = 1;							// Setting current widest to minimum of one
+					for(int g = 0; g < len; g++){			// looping through all members of struct
+						int width = 1;						// setting minimum width to 1
 						std::ostringstream str1;			// output string stream
 						str1 << content[g];					// sending number to output as string
 						std::string content = str1.str();	// converting to string
 						width = content.length(); 			// getting length
-						if(width > widest)
+						if(width > widest)					// checking if current member is widest
 							widest = width;
 					}
 					return widest;
@@ -89,10 +80,10 @@ namespace simplicity{
 			public:
 				template <typename T>
 				static void print(T content[], int length){
-					int width = widestMember(content, length);
-					printTop(length, width);
-					printContent(content, length, width);
-					printBottom(length,width);
+					int width = widestMember(content, length);	// finding the width to print
+					printTop(length, width);					// printing top of structure
+					printContent(content, length, width);		// printing content of struct
+					printBottom(length,width);					// printing bottom of struct and indexs
 				}
 				
 		
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main (void){
 
     std::cout<<"Cycle Intent: Visualize Arrays, Stacks, and Queues from a CS 240 student's program graphically, using ASCII characters, in a simple and easy-to-understand format\n";
 
-	double content[] = {626448.3,214748364.48,900,5,247.1579,354.000007};
+	int content[] = {626448,214748364,900,5,247,354};
 	int length = 6;
 
 	simplicity::simple::print(content,length); 
